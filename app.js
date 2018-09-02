5ffff522c291e010d8cb050357c575dd02cfe6bc
Eric Soares <contato.eric.sm@gmail.com>
2018-09-02 19:16:42 -0300

    Creating Directives

--- app.js
+++ app.js
@@ -23,43 +23,25 @@ myApp.config(function ($routeProvider){
 
 });
 
-myApp.service('nameService', function(){
+
+myApp.controller('mainController', ['$scope', '$log',
+function($scope, $log){
+
    
-    var self = this;
-    this.name = 'John Doe';
-    this.namelength = function(){
-        
-        return self.name.length;
-    }
-    
-});
-
-myApp.controller('mainController', ['$scope', '$log','nameService',
-function($scope, $log, nameService){
-
-   $scope.name = nameService.name;
-    
-    $scope.$watch('name', function(){
-       
-        nameService.name = $scope.name;
-    
-    });
-    
-    
-    $log.log(nameService.name);
-    $log.log(nameService.namelength());
   
 }]);
 
-myApp.controller('secondController', ['$scope','$log','$routeParams','nameService', function($scope,$log, $routeParams, nameService){
+myApp.controller('secondController', ['$scope','$log','$routeParams',
+function($scope, $log, $routeParams){
 
-    $scope.name = nameService.name;
-   $scope.num = $routeParams.num || 1;
+
     
-    $scope.$watch('name', function(){
-       
-        nameService.name = $scope.name;
-    
-    });
 }]);
-    
+
+myApp.directive("searchResult", function(){
+    return{
+        restrict: 'AECM',
+        template: ' <a href="#" class="list-group-item"><h4 class="list-group-item-heading">Doe, John</h4><p class="list-group-item-text">555 Main St., New York, NY 11111</p></a>',
+        replace: true
+    }
+});
